70245aa5d5e7ee40c018d4cffdd6ce84fbe5d7a4
fangbinwei <fangbinwei@yeah.net>
2019-03-14 03:27:25 +0800

    feat(plugin1): new feature

--- packages/fangbinwei-plugin-1-lerna/lib/plugin-1-lerna.js
+++ packages/fangbinwei-plugin-1-lerna/lib/plugin-1-lerna.js
@@ -5,8 +5,10 @@ module.exports = {
   name: 'plugin1 v3.2.1'
 }
 
-const change1 = 'plugin1 add new fea'
+const change1 = 'plugin1 add new feat'
 
 const change2 = 'plugin1 fix bug'
 
-const change2 = 'plugin1 fix bug2'
+const change3 = 'plugin1 fix bug2'
+
+const change4 = 'plugin1 new feat'
