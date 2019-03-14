2f30759ab5e2a31545fdceea605e0771c16b652b
fangbinwei <fangbinwei@yeah.net>
2019-03-15 00:57:30 +0800

    fix: plugin1
    fix bug

--- packages/fangbinwei-plugin-1-lerna/lib/plugin-1-lerna.js
+++ packages/fangbinwei-plugin-1-lerna/lib/plugin-1-lerna.js
@@ -4,7 +4,6 @@ console.log('plugin1')
 module.exports = {
   name: 'plugin1 v3.2.1'
 }
-
 const change1 = 'plugin1 add new feat'
 
 const change2 = 'plugin1 fix bug'
@@ -13,5 +12,4 @@ const change3 = 'plugin1 fix bug2'
 
 const change4 = 'plugin1 new feat'
 
-const change5 = 'plugin1 new feat'
 
